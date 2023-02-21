c52bec71f6373474cde92b0a71b1456b85ee1f56
Vivek Jadhav <67087656+vivekjadhav-vj@users.noreply.github.com>
2023-02-21 23:04:29 +0530

    final code

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -16,12 +16,12 @@ public class Workspace extends Gmail{
        this.calendar = new ArrayList<>();
     }
 
-    public void addMeeting(Meeting meeting){
+    public void addMeeting (Meeting meeting){
         //add the meeting to calendar
       calendar.add(meeting);
     }
 
-    public int findMaxMeetings(){
+    public int findMaxMeetings (){
         // find the maximum number of meetings you can attend
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
